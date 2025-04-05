fccfca87c350e27ef1e68c5c9e7a274cdf061d58
praveenmaila <praveenmaila249@gmail.com>
2025-04-05 14:41:52 +0530

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,5 @@
+def double(n):
+    return 2*n  #return twice the given number
+print("you can call this function using its name and assign the value of your you can run the python interpreterin the directory")
+print("this program ran  successfully")
+#----------------------------------------------------
---
+++ ex2-variables.py
@@ -0,0 +1,27 @@
+#variables
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+age = 10
+Age = 11
+AGE = 12
+print(AGE)
+
+#assign multiple values
+a,b,c = 'apple','banana','orange'
+print (a,b,c)
+a,b,c = 'apple','banana','orange'
+print (a,b,c,sep='\n')
+# one value to multiple variables
+a=b=c =10
+print(a,b,c)
+p = 10
+q = 7
+r = p*q
+print(r)
