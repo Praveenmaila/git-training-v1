eca34afc18f19383b4f3fa96398682b0dc1c6eda
demon <praveenmaila249@gmail.com>
2025-04-05 20:57:51 +0530

    Added a variable h = 200 from techdev branch

--- demofolder/function_ex.py
+++ demofolder/function_ex.py
@@ -1,1 +1,2 @@
-x = 500
+x = 600 #changed the value of x to 600
+z = 800
--- ex2-variables.py
+++ ex2-variables.py
@@ -33,3 +33,4 @@ print ("Added this line of code from Demon Salvoture from git terminal")
 print ("added this line from github for the first time")
 #---------------------------------------------------------------------
 print ("Added this line of code from techdev branch from git terminal")
+h = 200
